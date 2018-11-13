fb68c89f10ee1a3a4b59a0c19e114efd535c780b
azheng2010 <yaoyao12348@126.com>
2018-11-13 11:45:03 +0800

    Update util_install.sh

--- util_install.sh
+++ util_install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
 
 #安装一些常用的工具
-yum install -y tree
+yum install -y wget #安装wget下载工具
+yum install -y tree #查看目录树
+yum install -y tar unzip zip#压缩解压工具
+yum -y  install lsof #查看端口占用情况
+yum install -y bash-completion #使用systemctl自动补全服务名称
